ec204a707a0f4e47ba6c6fb036df7380046f43f8
Ruben <Ruben@live.dk>
2025-04-03 16:09:30 +0200

    Remove reliance on SimpleTheme. Refactor/rename.

---
+++ src/PicView.Avalonia/App.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace PicView.Avalonia;
+
+public partial class App : Application
+{
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
