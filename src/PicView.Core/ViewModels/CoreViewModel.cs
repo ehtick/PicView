bf8f2196eeaab7f5f35580bd9dd93fd0068f01ed
Ruben <Ruben2776@users.noreply.github.com>
2025-12-26 23:40:17 +0100

    Architecture planning

--- src/PicView.Core/ViewModels/CoreViewModel.cs
+++ src/PicView.Core/ViewModels/CoreViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using PicView.Core.IPlatform;
+using R3;
 
 namespace PicView.Core.ViewModels;
 
@@ -7,17 +8,38 @@ public class CoreViewModel
     public readonly IPlatformSpecificService? PlatformService;
     public readonly IPlatformWindowService? PlatformWindowService;
     
-    // Shared view models
+    // --- Shared view models ---
+    
     public TranslationViewModel Translation { get; } = new();
-    public GlobalSettingsViewModel GlobalSettings { get; } = new();
-    public SettingsViewModel? SettingsViewModel { get; set; }
-    //public GalleryViewModel SharedGallery { get; } = new();
-    public ExifViewModel? Exif { get; set; }
+    public GlobalSettingsViewModel GlobalSettings { get; } = new(); // Bindable reactive properties for the entire UI
+    public SettingsViewModel? SettingsViewModel { get; set; } // View model for the settings view (max 1 window)
+    
+    public SharedNavigationViewModel SharedNavigation { get; } = new();
+    
+    //public AboutViewModel? AboutView { get; set; }
+
     public KeybindingsViewModel? Keybindings { get; set; }
     //public WindowViewModel Window { get; } = new();
 
-
-    // Collection view models
+    // --- Collection of view models ---
+    
+    /// Tracks the correct position of the active window
+    public BindableReactiveProperty<int> MainWindowIndex  { get; } = new(); 
     public List<TabOverviewViewModel> TabsCollection { get; } = [];
+    /// Bindable reactive properties that are Exif related
+    public List<ExifViewModel> Exifs { get; } = []; 
+    /// View models for the image info view
+    public List<ImageInfoWindowViewModel?> InfoWindows { get; } = [];  
+    
+    public CoreViewModel(IPlatformSpecificService? platformSpecificService, IPlatformWindowService? platformWindowService)
+    {
+        PlatformService = platformSpecificService;
+        PlatformWindowService = platformWindowService;
+    }
+
+    public CoreViewModel()
+    {
+        // Only use for unit test
+    }
 
 }
---
+++ src/PicView.Core/ViewModels/SharedNavigationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PicView.Core.Navigation.Interfaces;
+
+namespace PicView.Core.ViewModels;
+
+public class SharedNavigationViewModel
+{
+    public INavigationService? NavigationService { get; private set; }
+    public IImageCache? SharedCache { get; private set; }
+
+    public void Initialize(INavigationService navigationService, IImageCache cache)
+    {
+        if (navigationService is null)
+        {
+            return;
+        }
+        NavigationService = navigationService;
+        SharedCache = cache;
+    }
+        
+}
