7fe0fb25427f36f4129f159391b8fb956adba3b1
Ruben <Ruben2776@users.noreply.github.com>
2026-01-16 18:32:34 +0100

    Refactor: Introduce `LanguageView2` for language selection, replace `LanguageSection` in `SettingsView2` with new component, and update project files to include `LanguageView2`.

---
+++ src/PicView.Avalonia/Views/Config/LanguageView2.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace PicView.Avalonia.Views.Config;
+
+public partial class LanguageView2 : UserControl
+{
+    public LanguageView2()
+    {
+        InitializeComponent();
+    }
+}
