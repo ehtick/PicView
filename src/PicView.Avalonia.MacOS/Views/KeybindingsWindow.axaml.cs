a5b8305cbea261b405d39a874b90eddea81c28c8
Ruben <ruben@live.dk>
2026-05-23 12:56:28 +0200

    Refactor `KeybindingsWindow` and `OpenWithView` to use updated `GenericWindow` logic and `MainWindowViewModel`

--- src/PicView.Avalonia.MacOS/Views/KeybindingsWindow.axaml.cs
+++ src/PicView.Avalonia.MacOS/Views/KeybindingsWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Input;
 using Avalonia.Media;
+using PicView.Avalonia.CustomControls;
 using PicView.Avalonia.UI;
 using PicView.Avalonia.WindowBehavior;
 using PicView.Core.Config;
@@ -10,14 +11,11 @@ using R3;
 
 namespace PicView.Avalonia.MacOS.Views;
 
-public partial class KeybindingsWindow : Window, IDisposable
+public partial class KeybindingsWindow : GenericWindow
 {
-    private readonly IDisposable _disposable;
-    private readonly KeybindingWindowConfig _config;
 
     public KeybindingsWindow(KeybindingWindowConfig config)
     {
-        _config = config;
         InitializeComponent();
         if (Settings.Theme.GlassTheme)
         {
@@ -28,41 +26,6 @@ public partial class KeybindingsWindow : Window, IDisposable
         {
             XKeybindingsView.Background = UIHelper.GetMenuBackgroundColor();
         }
-        GenericWindowHelper.KeybindingsWindowInitialize(this);
-
-        _disposable = ClientSizeProperty.Changed.ToObservable()
-            .ObserveOn(UIHelper.GetFrameProvider)
-            .Subscribe(UpdateWindowSize);
-        PositionChanged += (_, _) => UpdateWindowPosition();
-        
-        Closing += async delegate
-        {
-            Hide();
-            if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                var hostWindow = desktop.MainWindow;
-                hostWindow?.Focus();
-            }
-            await _config.SaveAsync();
-        };
-    }
-
-    private void UpdateWindowSize(AvaloniaPropertyChangedEventArgs<Size> size)
-        => WindowFunctions.SetWindowSize(this, size, _config.WindowProperties);
-
-    private void UpdateWindowPosition()
-    {
-        _config.WindowProperties.Left = Position.X;
-        _config.WindowProperties.Top = Position.Y;
-    }
-
-    private void MoveWindow(object? sender, PointerPressedEventArgs e)
-    {
-        BeginMoveDrag(e);
-    }
-
-    public void Dispose()
-    {
-        _disposable?.Dispose();
+        GenericWindowHelper.KeybindingsWindowInitialize(this, config.WindowProperties);
     }
 }
--- src/PicView.Avalonia.MacOS/Views/OpenWithView.axaml.cs
+++ src/PicView.Avalonia.MacOS/Views/OpenWithView.axaml.cs
@@ -5,9 +5,10 @@ using Avalonia.Interactivity;
 using Avalonia.Layout;
 using Avalonia.Threading;
 using PicView.Avalonia.UI;
-using PicView.Avalonia.ViewModels;
+using PicView.Avalonia.Views.UC;
 using PicView.Core.MacOS.AppLauncher;
 using PicView.Core.MacOS.FileAssociation;
+using PicView.Core.ViewModels;
 
 namespace PicView.Avalonia.MacOS.Views;
 
@@ -51,11 +52,11 @@ public partial class OpenWithView : Window
     {
         if (_filePath is null)
         {
-            if (DataContext is not MainViewModel vm)
+            if (DataContext is not MainWindowViewModel vm)
             {
                 return;
             }
-            _filePath = vm.PicViewer?.FileInfo?.CurrentValue.FullName;
+            _filePath = vm.WindowTabs.ActiveTab.CurrentValue.FileInfo.CurrentValue?.FullName;
         }
 
 
