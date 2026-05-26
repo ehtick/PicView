b1e855ce9311292ef96c667f0da15a94c00f2374
Ruben <Ruben2776@users.noreply.github.com>
2026-05-26 17:20:39 +0200

    Remove `SaveImageHandler` and refactor `ConvertView` to use `MagickImage` for file saving.

--- src/PicView.Avalonia/Views/Main/ConvertView.axaml.cs
+++ src/PicView.Avalonia/Views/Main/ConvertView.axaml.cs
@@ -1,13 +1,14 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using ImageMagick;
 using PicView.Avalonia.CustomControls;
+using PicView.Avalonia.FileSystem;
 using PicView.Core.ViewModels;
 
 namespace PicView.Avalonia.Views.Main;
 
 public partial class ConvertView : UserControl
 {
-    // TODO: refactor to new image file saving system
     public ConvertView()
     {
         InitializeComponent();
@@ -19,30 +20,25 @@ public partial class ConvertView : UserControl
                 return;
             }
         
-            SaveButton.Click += async (_, _) =>
+            SaveButton.Click += (_, _) =>
             {
-                // var destination = vm.PicViewer.FileInfo.CurrentValue.FullName;
-                // var ext = DetermineFileExtension(vm, ref destination);
-                //
-                // await SaveImageHandler.SaveImageWithPossibleNavigation(vm, vm.PicViewer.FileInfo.CurrentValue.FullName,
-                //     destination, true, ext);
+                var source = vm.WindowTabs.ActiveTab.CurrentValue.FileInfo.CurrentValue.FullName;
+                using var magick = new MagickImage(source);
+                var destination = GetDestinationWithChangedExtension(source);
+                magick.Write(destination);
             };
         
             SaveAsButton.Click += async (_, _) =>
             {
-                // var ext = GetExtension();
-                // var destination =
-                //     await FilePicker.PickFileForSavingAsync(vm.PicViewer.FileInfo?.CurrentValue.FullName, ext);
-                // if (destination is null)
-                // {
-                //     return;
-                // }
-                //
-                // var sameFile = destination.Equals(vm.PicViewer.FileInfo.CurrentValue.FullName,
-                //     StringComparison.OrdinalIgnoreCase);
-                //
-                // await SaveImageHandler.SaveImageWithPossibleNavigation(vm, vm.PicViewer.FileInfo.CurrentValue.FullName,
-                //     destination, sameFile, ext);
+                var source = vm.WindowTabs.ActiveTab.CurrentValue.FileInfo.CurrentValue.FullName;
+                var suggestedFileName = GetDestinationWithChangedExtension(source);
+                var file =  await FilePicker2.PickFileForSavingAsync(suggestedFileName);
+                if (file is null)
+                {
+                    return;
+                }
+                using var magick = new MagickImage(source);
+                magick.Write(file);
             };
         
             CancelButton.Click += (_, _) => SafeClose();
@@ -61,53 +57,45 @@ public partial class ConvertView : UserControl
         });
     }
 
-    private string GetExtension() => ConversionComboBox.SelectedIndex switch
+    private string GetDestinationWithChangedExtension(string source)
     {
-        1 => ".png",
-        2 => ".jpg",
-        3 => ".webp",
-        4 => ".avif",
-        5 => ".heic",
-        6 => ".jxl",
-        _ => ""
-    };
-
-    private string DetermineFileExtension(MainWindowViewModel vm, ref string destination)
-    {
-        // var ext = vm.PicViewer.FileInfo.CurrentValue.Extension;
-        // if (NoConversion.IsSelected)
-        // {
-        //     return ext;
-        // }
-        //
-        // if (PngItem.IsSelected)
-        // {
-        //     ext = ".png";
-        // }
-        // else if (JpgItem.IsSelected)
-        // {
-        //     ext = ".jpg";
-        // }
-        // else if (WebpItem.IsSelected)
-        // {
-        //     ext = ".webp";
-        // }
-        // else if (AvifItem.IsSelected)
-        // {
-        //     ext = ".avif";
-        // }
-        // else if (HeicItem.IsSelected)
-        // {
-        //     ext = ".heic";
-        // }
-        // else if (JxlItem.IsSelected)
-        // {
-        //     ext = ".jxl";
-        // }
-        //
-        // destination = Path.ChangeExtension(destination, ext);
-        // return ext;
-        return "";
+        if (NoConversion.IsSelected)
+        {
+            return source;
+        }
+        string ext;
+        
+        if (PngItem.IsSelected)
+        {
+            ext = ".png";
+        }
+        else if (JpgItem.IsSelected)
+        {
+            ext = ".jpg";
+        }
+        else if (WebpItem.IsSelected)
+        {
+            ext = ".webp";
+        }
+        else if (AvifItem.IsSelected)
+        {
+            ext = ".avif";
+        }
+        else if (HeicItem.IsSelected)
+        {
+            ext = ".heic";
+        }
+        else if (JxlItem.IsSelected)
+        {
+            ext = ".jxl";
+        }
+        else
+        {
+            return source;
+        }
+        
+        source = Path.ChangeExtension(source, ext);
+        return source;
     }
 
     private void BottomBorder_OnPointerPressed(object? sender, PointerPressedEventArgs e)
